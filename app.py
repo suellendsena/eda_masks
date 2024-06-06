a21d43707a5b2b8758ed4eaaa6c7fff430cb82ba
unknown <s177261@dac.unicamp.br>
2024-06-06 15:02:53 -0300

    Ajustar shift

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def main():
 
     radius = st.sidebar.slider('Resolução para perfil', min_value=0.01, max_value=0.49, value=0.49, step=0.01)
     smooth = st.sidebar.slider('Smooth', min_value=100, max_value=1000, value=700, step=100)
-    shift = st.sidebar.slider('Shift do Gráfico de Ângulo', min_value=-100, max_value=100, value=0, step=10)  # Slider para deslocamento
+    shift = st.sidebar.slider('Shift', min_value=-250, max_value=250, value=0, step=10)  # Slider para deslocamento
     
     directory_path = f"data/{selected_patient}"
     file_list = os.listdir(directory_path)
@@ -186,7 +186,7 @@ def main():
                         ax.set_ylabel("Ângulo (graus)")
                         ax.legend()
                         st.pyplot(fig)
-                        
+
     if all_coordinates:
         coords_int = np.array([(round(x), round(y)) for x, y, radius in all_coordinates], dtype=int)
         coords_array = np.array(all_coordinates)
