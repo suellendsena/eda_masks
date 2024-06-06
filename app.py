2c3c3d1de9ca548a55f645fe0a4af4dc29b4754b
unknown <s177261@dac.unicamp.br>
2024-06-06 14:04:57 -0300

    Incluir spline

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def main():
     selected_patient = st.sidebar.selectbox('Selecione o Paciente:', patients)
 
     radius = st.sidebar.slider('Resolução para perfil', min_value=0.01, max_value=0.49, value=0.49, step=0.01)
-    smooth = st.sidebar.slider('Smooth', min_value=100, max_value=100, value=700, step=100)
+    smooth = st.sidebar.slider('Smooth', min_value=100, max_value=1000, value=700, step=100)
     
     directory_path = f"data/{selected_patient}"
     file_list = os.listdir(directory_path)
